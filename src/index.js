faabe6e75fd1432686bbce9dbff26ba4f8bd2457
martinski74 <martin.dobr@abv.bg>
2025-05-08 22:15:54 +0300

    up

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const { configRoutes } = require('./config/routes');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 async function start() {
   const app = express();
